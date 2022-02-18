54a14c8db9f2f43b26d50c505ec78e19cc930280
Takahisa Hirano <54578410+Hiratto@users.noreply.github.com>
2022-02-18 20:59:07 +0900

    配列に分割代入

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 // 分割代入
 
-const myPlofile = {
-  name: "Hiratto",
-  age: 35
-};
+// const myPlofile = {
+//   name: "Hiratto",
+//   age: 35
+// };
 
-const message1 = `名前は${myPlofile.name}です。年齢は${myPlofile.age}歳です。`;
-console.log(message1);
+// const message1 = `名前は${myPlofile.name}です。年齢は${myPlofile.age}歳です。`;
+// console.log(message1);
 
-// テンプレートリテラルの中身が短くて済む
-const { name, age } = myPlofile;
-const message2 = `名前は${name}です。年齢は${age}歳です。`;
-console.log(message2);
+// // テンプレートリテラルの中身が短くて済む
+// const { name, age } = myPlofile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+// 配列に分割代入
+
+// const myPlofile = ['Hiratto',35];
+// const message3 = `名前は${myPlofile[0]}です。年齢は${myPlofile[1]}歳です。`;
+// console.log(message3);
+
+// const [name,age] = myPlofile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
